c8cadb7aa2f514a89abc27d79a95bb98d4ac4f71
Kevin Sousa <kevinsmoura@hotmail.com>
2021-03-22 20:15:36 -0300

    Eureka server configuration

--- src/main/java/br/com/microsevice/loja/service/CompraService.java
+++ src/main/java/br/com/microsevice/loja/service/CompraService.java
@@ -1,5 +1,6 @@
 package br.com.microsevice.loja.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -11,13 +12,16 @@ import br.com.microsevice.loja.controller.dto.InfoFornecedorDTO;
 @Service
 public class CompraService {
 
+	@Autowired
+	private RestTemplate client;
+	
 	public void realizaCompra(CompraDTO compra) {
 		
-		RestTemplate client = new RestTemplate();
-		ResponseEntity<InfoFornecedorDTO> exchange = client.exchange("http://localhost:8081/info/"+compra.getEndereco().getEstado(), HttpMethod.GET, 
+		ResponseEntity<InfoFornecedorDTO> exchange = 
+				client.exchange("http://fornecedor/info/"+compra.getEndereco().getEstado(), HttpMethod.GET, 
 				null, InfoFornecedorDTO.class);
 		
-		System.out.println(exchange.getBody());
+		System.out.println(exchange.getBody().getEndereco());
 	}
 
 }
